234f5b7f0d80ec9a6fd05ecee24d8c081b2ae4bf
patbal <supatbal@yahoo.fr>
2018-03-22 09:58:33 +0100

    initial commit

---
+++ src/Controller/mainController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: patbal
+ * Date: 22/03/2018
+ * Time: 09:39
+ */
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class mainController extends AbstractController
+{
+
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('first but not last');
+    }
+
+    /**
+     * @Route("tt/{slug}/")
+     */
+    public function testslug($slug)
+    {
+        return new Response('tu as tapé '.$slug.' dans la barre, hein ?');
+    }
+
+
+}
